24e50d55799e7e31460ad57c9ce6be016d9c8f02
Lucia Mormocea <lucia.mormocea@laterooms.com>
2018-10-27 18:54:06 +0100

    Sending sms now

--- PenelopeApi/middleware/temperature.js
+++ PenelopeApi/middleware/temperature.js
@@ -1,7 +1,23 @@
 const bodyParser = require('body-parser');
+var clockwork = require("clockwork")({key:"b1dacda2b2ff39cfa6c1bd6915d91d89adc9a1c3"});
 
- function setTemperature(req, res){
-    console.log(req);    
+const phoneNumber = "447780542460";
+const threshholdTemp = 35;
+
+function sendSms(message){
+    clockwork.sendSms({ To: phoneNumber, Content: message}, function(error, resp) {
+        if (error) {
+            console.log("Something went wrong", error);
+        } else {
+            console.log("Message sent",resp.responses[0].id);
+        }
+    });
+}
+
+function setTemperature(req, res){
+    
+    if(req.params.t >= threshholdTemp )
+
     res.setHeader('Content-Type', 'text/plain');
     res.write('you posted:\n');
     res.end(JSON.stringify(req.params.p, null, 2));   
