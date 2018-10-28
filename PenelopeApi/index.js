fdfd524c87c882a3a7317b6345402fd238722a28
Lucia Mormocea <lucia.mormocea@laterooms.com>
2018-10-28 04:54:09 +0000

    cross origin fix

--- PenelopeApi/index.js
+++ PenelopeApi/index.js
@@ -12,6 +12,22 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    //intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+      //respond with 200
+      res.send(200);
+    }
+    else {
+    //move on
+      next();
+    }
+});
+
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/temperature/:t', (req, res) => temperature.setTemperature(req,res, cache));
 app.get('/humidity/:h', (req, res) => humidity.setHumidity(req,res, cache));
