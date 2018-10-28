fec4e010a72e1853d0f8f934a88dba67906bcbcf
Lucia Mormocea <lucia.mormocea@laterooms.com>
2018-10-28 03:32:20 +0000

    Had to shorten heroku link

--- PenelopeApi/middleware/messageEngine.js
+++ PenelopeApi/middleware/messageEngine.js
@@ -3,13 +3,13 @@ const temperatureMessages = [ "Have we moved to hell again?",
                             "Hey, do you know what would be great? NOT burning Alive!", 
                             "I'm not a F****ing cactus!",
                             "Are you too poor or too cheap to install Air-Con?" ];
-const keirUrl = " https://green-coders.herokuapp.com/";
+const keirUrl = "https://bit.ly/2z6a5Da";//"https://green-coders.herokuapp.com/";
 
 function getMessage(topic, sensorValue){
     if(topic == 'temperature')
     {
         let msgIndex = random(0, temperatureMessages.length -1, 1);        
-        let msg = temperatureMessages[msgIndex] + "(" + sensorValue + "C)" + keirUrl;
+        let msg = temperatureMessages[msgIndex] + "(" + sensorValue + "C) " + keirUrl;
         return msg;
     }
 }
--- PenelopeApi/middleware/sms.js
+++ PenelopeApi/middleware/sms.js
@@ -3,12 +3,15 @@ const phoneNumber = "447780542460";
 var clockwork = require("clockwork")({key:"b1dacda2b2ff39cfa6c1bd6915d91d89adc9a1c3"});
 
 function send(message){
-    clockwork.sendSms({ To: phoneNumber, Content: message, From: "Penelope"}, function(error, resp) {
+    clockwork.sendSms({ To: phoneNumber, Content: message}, function(error, resp) {        
         if (error) {
             console.log("Something went wrong", error);
         } else {
-            console.log("Message sent",resp.responses[0].id);
-        }
+            if(resp.responses[0].success)
+                console.log("Message sent",resp.responses[0].id);
+            else 
+            console.log("Something went wrong", resp.responses[0]);
+        };
     });
 }
 
