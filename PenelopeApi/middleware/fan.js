889fd6beeb192c76ab4fad696aa3b940551bb97d
Lucia Mormocea <lucia.mormocea@laterooms.com>
2018-10-28 12:53:14 +0000

    Calling the pi to start the fan

--- PenelopeApi/middleware/fan.js
+++ PenelopeApi/middleware/fan.js
@@ -1,9 +1,24 @@
+const sms = require("./sms");
+const timing = require("./timing");
 
-function start(req,res){
+function start(req,res, cache){
+    
     console.log('started fan');
     
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Fan started \n");   
+    if(timing.canSendSms(cache))
+        {
+            var message = "FAN";
+            sms.sendSmsToPie(message);
+            timing.update(cache);
+
+            res.write("Triggered fan");  
+            res.end(); 
+            return;
+        }
+        else{            
+            res.end(JSON.stringify("Too many messages sent recently", null, 2)); 
+            return;
+        }    
 }
 
 exports.start = start;
--- PenelopeApi/middleware/sms.js
+++ PenelopeApi/middleware/sms.js
@@ -1,5 +1,6 @@
 
 const phoneNumber = "447895146077";
+const pieNumber = "447860033076";
 var clockwork = require("clockwork")({key:"b1dacda2b2ff39cfa6c1bd6915d91d89adc9a1c3"});
 
 function send(message){
@@ -15,4 +16,18 @@ function send(message){
     });
 }
 
+function sendSmsToPie(message){
+    clockwork.sendSms({ To: pieNumber, Content: message, From: "Penelope"}, function(error, resp) {        
+        if (error) {
+            console.log("Something went wrong", error);
+        } else {
+            if(resp.responses[0].success)
+                console.log("Message sent",resp.responses[0].id);
+            else 
+            console.log("Something went wrong", resp.responses[0]);
+        };
+    });
+}
+
 exports.send = send;
+exports.sendSmsToPie = sendSmsToPie;
