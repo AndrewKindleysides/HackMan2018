223f0d03c9a86e0b84ddc9484bbdaaecc666b71b
Lucia Mormocea <lucia.mormocea@laterooms.com>
2018-10-28 12:59:39 +0000

    Removed throtle off the fan

--- PenelopeApi/middleware/fan.js
+++ PenelopeApi/middleware/fan.js
@@ -2,23 +2,23 @@ const sms = require("./sms");
 const timing = require("./timing");
 
 function start(req,res, cache){
-    
+
     console.log('started fan');
     
-    if(timing.canSendSms(cache))
-        {
+    //if(timing.canSendSms(cache))
+        //{
             var message = "FAN";
             sms.sendSmsToPie(message);
-            timing.update(cache);
+            //timing.update(cache);
 
             res.write("Triggered fan");  
             res.end(); 
             return;
-        }
-        else{            
-            res.end(JSON.stringify("Too many messages sent recently", null, 2)); 
-            return;
-        }    
+        //}
+        // else{            
+        //     res.end(JSON.stringify("Too many messages sent recently", null, 2)); 
+        //     return;
+        // }    
 }
 
 exports.start = start;
